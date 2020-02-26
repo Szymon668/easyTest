6e8515bb6a07284f981b2fef0255cb8c47d70259
Szymon Zygas <Szymonz668@gmail.com>
2020-02-26 09:39:20 +0100

    small refactor

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -3,31 +3,28 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 
 public class Reader {
 
 
-    public void read(File data) throws IOException {
-        List<String> lines= new ArrayList<>();
+    public List<String> read(File data) throws IOException {
+        List<String> lines = new ArrayList<>();
         try (Scanner scanner = new Scanner(data)) {
-            String createdString="";
+            String createdString = "";
             while (scanner.hasNext()) {
-                String nextLine=scanner.nextLine();
-              createdString+=nextLine;
+                String nextLine = scanner.nextLine();
+                createdString += nextLine;
 
-        }
-            StringTokenizer ss=new StringTokenizer(createdString,"WEB");
+            }
             String[] webs = createdString.split("(?=WEB)");
-            for (String s:webs
-                 ) {
-                System.out.println(s);
+            for (String s : webs) {
+                lines.add(s);
             }
 
+        }
+        return lines;
+
     }
-
-
-}
 
 }
