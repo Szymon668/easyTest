6818b795cfe63bf8ea84c2fa8849799f4009d27d
szymon <szymonz668@gmail.com>
2020-02-28 15:38:52 +0100

    good version with 3 tests

--- src/main/java/Verificator.java
+++ src/main/java/Verificator.java
@@ -13,15 +13,17 @@ public class Verificator {
         return true;
     }
     public boolean verifyWithCondition(List<String> data,List<String> conditions, List<String> output) {
-        List<String >linesBeforeCondition=new ArrayList<>();
+
         for (int i=1;i<conditions.size();i+=2) {
+            List<String >linesBeforeCondition=new ArrayList<>();
             for (String s1:output) {
-                linesBeforeCondition.add(s1);
+                System.out.println(conditions.get(i));
                 if(s1.equals(conditions.get(i))){
                    if(!linesBeforeCondition.contains(conditions.get(i-1))){
                        return false;
                    }
                 }
+                linesBeforeCondition.add(s1);
             }
         }
         for (String s : data) {
---
+++ src/main/test/VerifyWithConditionTest.java
@@ -0,0 +1,35 @@
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class VerifyWithConditionTest extends TestCase {
+    public void testWithEmptyMetadata() throws IOException {
+        Reader reader = new Reader();
+        List<String> data = reader.read(new File("data.txt"));
+        List<String> output = reader.read(new File("output.txt"));
+        List<String> metadata = reader.readMetaData(new File("metadata.txt"));
+        Verificator verificator=new Verificator();
+        boolean isTestPassed = verificator.verifyWithCondition(data, metadata, output);
+        assertTrue(isTestPassed);
+    }
+    public void testWithCorrectMetadata() throws IOException {
+        Reader reader = new Reader();
+        List<String> data = reader.read(new File("data.txt"));
+        List<String> output = reader.read(new File("output.txt"));
+        List<String> metadata = reader.readMetaData(new File("metadata1.txt"));
+        Verificator verificator=new Verificator();
+        boolean isTestPassed = verificator.verifyWithCondition(data, metadata, output);
+        assertTrue(isTestPassed);
+    }
+    public void testWithNotCorrectMetadata() throws IOException {
+        Reader reader = new Reader();
+        List<String> data = reader.read(new File("data.txt"));
+        List<String> output = reader.read(new File("output.txt"));
+        List<String> metadata = reader.readMetaData(new File("metadata2.txt"));
+        Verificator verificator=new Verificator();
+        boolean isTestPassed = verificator.verifyWithCondition(data, metadata, output);
+        assertFalse(isTestPassed);
+    }
+}
